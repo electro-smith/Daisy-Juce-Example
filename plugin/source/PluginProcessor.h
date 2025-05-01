ee1590f52970276119b35d420378cb19a27ca544
Joel Wetzell <jwetzell@yahoo.com>
2025-05-01 15:23:32 -0500

    update DaisySP submodule

--- plugin/source/PluginProcessor.h
+++ plugin/source/PluginProcessor.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <juce_audio_processors/juce_audio_processors.h>
-#include "../DaisySP/daisysp.h"
+#include "daisysp.h"
 
 //==============================================================================
 class AudioPluginAudioProcessor : public juce::AudioProcessor
